55a2adb70974ecb2ace0e1e44f78e2724d87fa74
Benbb96 <benbb96@gmail.com>
2023-04-10 01:11:47 +0200

    Add SECRET_KEY to env variable

--- demo/demodesk/config/settings.py
+++ demo/demodesk/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_crkn1+fnzu5$vns_-d+^ayiq%z4k*s!!ag0!mfy36(y!vrazd'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='_crkn1+fnzu5$vns_-d+^ayiq%z4k*s!!ag0!mfy36(y!vrazd')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
